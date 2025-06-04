5b8fe146190b066e6cb04423a61977b3daf3ccfc
Yash Yadav <yashyadav@Yashs-MacBook-Air-3.local>
2025-06-05 00:13:56 +0530

    cors issue resolved

--- mechat-backend/constants/config.js
+++ mechat-backend/constants/config.js
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config(); 
+
 const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
+    // "https://talktalknow.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
